03c9c3043ffc2aca64568cec163b39bad6cb633a
NguyenSon <asanfc9x@gmail.com>
2017-12-12 23:05:13 +0700

    add field for NhomNganh

--- DTO/DTO_NhomNganh.cs
+++ DTO/DTO_NhomNganh.cs
@@ -8,16 +8,17 @@ namespace DTO
     public class DTO_NhomNganh
     {
         private int nhom_nganh;
-        private string ten_chuyen_nganh;
+        private string ten_chuyen_nganh, mo_ta;
 
         public DTO_NhomNganh()
         {
         }
 
-        public DTO_NhomNganh(int ma_nhom_nganh, string ten_chuyen_nganh)
+        public DTO_NhomNganh(int ma_nhom_nganh, string ten_chuyen_nganh, string mo_ta)
         {
             this.nhom_nganh = ma_nhom_nganh;
             this.ten_chuyen_nganh = ten_chuyen_nganh;
+            this.mo_ta = mo_ta;
         }
 
         public int MaNhomNganh
@@ -41,6 +42,18 @@ namespace DTO
             set
             {
                 ten_chuyen_nganh = value;
+            }
+        }
+
+        public string MoTa
+        {
+            get
+            {
+                return mo_ta;
+            }
+            set
+            {
+                mo_ta = value;
             }
         }
     }
