6d6ee3617897cd9291af0982017028bd27d51a02
NguyenSon <asanfc9x@gmail.com>
2017-12-12 23:13:55 +0700

    modify DTO_NhomNganh

--- DTO/DTO_NhomNganh.cs
+++ DTO/DTO_NhomNganh.cs
@@ -8,17 +8,16 @@ namespace DTO
     public class DTO_NhomNganh
     {
         private int nhom_nganh;
-        private string ten_chuyen_nganh, mo_ta;
+        private string ten_chuyen_nganh;
 
         public DTO_NhomNganh()
         {
         }
 
-        public DTO_NhomNganh(int ma_nhom_nganh, string ten_chuyen_nganh, string mo_ta)
+        public DTO_NhomNganh(int ma_nhom_nganh, string ten_chuyen_nganh)
         {
             this.nhom_nganh = ma_nhom_nganh;
             this.ten_chuyen_nganh = ten_chuyen_nganh;
-            this.mo_ta = mo_ta;
         }
 
         public int MaNhomNganh
@@ -42,18 +41,6 @@ namespace DTO
             set
             {
                 ten_chuyen_nganh = value;
-            }
-        }
-
-        public string MoTa
-        {
-            get
-            {
-                return mo_ta;
-            }
-            set
-            {
-                mo_ta = value;
             }
         }
     }
